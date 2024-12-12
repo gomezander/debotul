be77ac15eb004872e2ecab5a1bf1bb742f148593
xm0d4r <joelorella@gmail.com>
2024-12-12 17:21:08 +0100

    UX changes

--- core/menu.py
+++ core/menu.py
@@ -1,7 +1,13 @@
+from __future__ import print_function
+
 import time
 import random
 
-banner = [r'''                , ,, ,                              
+
+import sys
+import time
+
+dibujo = [r'''                , ,, ,                              
                 | || |    ,/  _____  \.             
                 \_||_/    ||_/     \_||             
                   ||       \_| . . |_/              
@@ -38,7 +44,7 @@ banner = [r'''                , ,, ,
                 /\/~~\-/'        _,-'                     |     /' /'  `
                 |_`\~~~/`\     /~                          \/~~' /'
                    |`\~ \ `\   `\                           `| /'
-    ''', 
+    ''',
     r'''
                               .-"/   .-"/
                              /  (.-./  (
@@ -67,33 +73,69 @@ banner = [r'''                , ,, ,
     '''
 ]
 
+banner = (r"""
+        ▓█████▄  ██▓ ▄▄▄       ▄▄▄▄    ██▓     ▒█████  
+        ▒██▀ ██▌▓██▒▒████▄    ▓█████▄ ▓██▒    ▒██▒  ██▒
+        ░██   █▌▒██▒▒██  ▀█▄  ▒██▒ ▄██▒██░    ▒██░  ██▒
+        ░▓█▄   ▌░██░░██▄▄▄▄██ ▒██░█▀  ▒██░    ▒██   ██░
+        ░▒████▓ ░██░ ▓█   ▓██▒░▓█  ▀█▓░██████▒░ ████▓▒░
+        ▒▒▓  ▒ ░▓   ▒▒   ▓▒█░░▒▓███▀▒░ ▒░▓  ░░ ▒░▒░▒░ 
+        ░ ▒  ▒  ▒ ░  ▒   ▒▒ ░▒░▒   ░ ░ ░ ▒  ░  ░ ▒ ▒░ 
+        ░ ░  ░  ▒ ░  ░   ▒    ░    ░   ░ ░   ░ ░ ░ ▒  
+        ░     ░        ░  ░ ░          ░  ░    ░ ░  
+        ░                          ░                """)
+
+class colors:
+    CRED2 = "\33[91m"
+    CBLUE2 = "\33[94m"
+    ENDC = "\033[0m"
+
+
 def show_menu():
+
+    for col in banner:
+        print(colors.CRED2 + col, end="")
+        sys.stdout.flush()
+        time.sleep(0.0025)
+
+    x = ("""
+                    Authors:  xm0d, agarma, at0mic, ander
+                    """)
+    for col in x:
+        print(colors.CBLUE2 + col, end="")
+        sys.stdout.flush()
+        time.sleep(0.0040)
+
+    y = "\n\t\t          😈 El diablo!😈\n"
+    for col in y:
+        print(colors.CRED2 + col, end="")
+        sys.stdout.flush()
+        time.sleep(0.0040)
+
+    z = "\n"
+    for col in z:
+        print(colors.ENDC + col, end="")
+        sys.stdout.flush()
+        time.sleep(0.4)
+
     """
     Muestra un menú interactivo para que el usuario elija el tipo de scan.
     """
-    print(r"""
-
- ______    ________  ________    _______   __       ______      
-/_____/\  /_______/\/_______/\ /_______/\ /_/\     /_____/\     
-\:::_ \ \ \__.::._\/\::: _  \ \\::: _  \ \\:\ \    \:::_ \ \    
- \:\ \ \ \   \::\ \  \::(_)  \ \\::(_)  \/_\:\ \    \:\ \ \ \   
-  \:\ \ \ \  _\::\ \__\:: __  \ \\::  _  \ \\:\ \____\:\ \ \ \  
-   \:\/.:| |/__\::\__/\\:.\ \  \ \\::(_)  \ \\:\/___/\\:\_\ \ \ 
-    \____/_/\________\/ \__\/\__\/ \_______\/ \_____\/ \_____\/ 
-                                                                
-                            El diablo!""")
+    for col in random.choice(dibujo):
+        print(colors.CRED2 + col, end="")
+        sys.stdout.flush()
+        #time.sleep(0.0025)
     time.sleep(1)
-    print(random.choice(banner))
-    time.sleep(1)
-    print("Seleccione el tipo de reconocimiento que desea realizar:")
-    print("1. Internal Recon")
-    print("2. External Recon")
-    print("3. HTTP")
-    print("4. Custom")
-    print("5. Exit")
+    print("\n" + colors.ENDC + col)
+    print("Seleccione el tipo de reconocimiento que desea realizar:\n")
+    print(" 1. Internal Recon")
+    print(" 2. External Recon")
+    print(" 3. HTTP")
+    print(" 4. Custom")
+    print(" 5. Exit")
 
     # Solicitar al usuario que seleccione un perfil
-    choice = input("Ingrese el número correspondiente a su elección: ")
+    choice = input("\nIngrese el número correspondiente a su elección: ")
 
     profiles = {
         '1': 'Internal Recon',
