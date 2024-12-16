4db26a1a842f3207d490b3977f246ac93dd9eb99
xm0d4r <joelorella@gmail.com>
2024-12-16 16:40:17 +0100

    broad

--- modules/enum4linux.py
+++ modules/enum4linux.py
@@ -1,14 +1,26 @@
 from core import execute_command, save_output_to_file, clean_url
 from core import RESULTS_DIRECTORY, RESULTS_FILEEXTENSION
+from datetime import datetime
 
 def execute_enum4linux(target):
     """
     Ejecuta Enum4linux para enumerar información de un sistema Windows.
     """
-    
+    # Establecer el tiempo de inicio
+    start_time = datetime.now()
+
+    original_target = target
+    target = clean_url(target)
+
     # Ejecutar Enum4linux
     command = f"enum4linux -a {target}"
     result = execute_command(command)
     
+    # Modificar path para guardar el fichero
+    RESULTS_FOLDERPATH = RESULTS_DIRECTORY+'/'+ target+'/'
+
     # Guardar el resultado en un archivo
-    save_output_to_file(result, RESULTS_DIRECTORY + RESULTS_FILEEXTENSION)
+    save_output_to_file(result, RESULTS_FOLDERPATH + target+'_enum4linux'+ RESULTS_FILEEXTENSION, target,start_time)
+
+    # Restaurar el target original después de _enum4linux
+    target = original_target
--- modules/netexec.py
+++ modules/netexec.py
@@ -1,14 +1,26 @@
-from core import execute_command, save_output_to_file
-from core import RESULTS_DIRECTORY
+from core import execute_command, save_output_to_file, clean_url
+from core import RESULTS_DIRECTORY, RESULTS_FILEEXTENSION
+from datetime import datetime
 
 def execute_netexec(target):
     """
     Ejecuta Netexec para obtener información sobre la red SMB en el target.
     """
-    
+    # Establecer el tiempo de inicio
+    start_time = datetime.now()
+
+    original_target = target
+    target = clean_url(target)
+
     # Ejecutar Netexec (comando SMB)
-    command = f"netexec {target}"
+    command = f"netexec smb {target} -u '' -p '' --shares"
     result = execute_command(command)
     
+    # Modificar path para guardar el fichero
+    RESULTS_FOLDERPATH = RESULTS_DIRECTORY+'/'+ target+'/'
+
     # Guardar el resultado en un archivo
-    save_output_to_file(result, RESULTS_DIRECTORY + 'test')
+    save_output_to_file(result, RESULTS_FOLDERPATH + target+'_netexec'+ RESULTS_FILEEXTENSION, target,start_time)
+
+    # Restaurar el target original después de _enum4linux
+    target = original_target
